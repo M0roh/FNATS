07f32f222eb6f3aaaf4392984fbdc516ad77b487
Морон . <sisandr333@gmail.com>
2025-09-03 12:58:04 +0300

    Added office manager script (#54670034)

--- Assets/Scripts/Core/OfficeManager.cs
+++ Assets/Scripts/Core/OfficeManager.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using VoidspireStudio.FNATS.Interactables;
 
 namespace VoidspireStudio.FNATS.Core
 {
     public class OfficeManager : MonoBehaviour
     {
+        [SerializeField] private LightSwitcher _officeLight;
+        [SerializeField] private Door _officeDoor;
 
+        public bool IsPlayerInOffice { get; private set; } = false;
+        public bool IsPlayerUnderTable { get; private set; } = false;
+        public bool IsPlayerNearOffice { get; private set; } = false;
+        public bool IsOpenedDoor => _officeDoor.IsOpen;
+        public bool IsEnabledLight => _officeLight.IsActive;
+
+        public void PlayerHideUnderTable() => IsPlayerUnderTable = true;
+        public void PlayerUnhideUnderTable() => IsPlayerUnderTable = false;
+
+        public void PlayerEnterOffice() => IsPlayerInOffice = true;
+        public void PlayerExitOffice() => IsPlayerInOffice = false;
+
+        public void PlayerNearOffice() => IsPlayerNearOffice = true;
+        public void PlayerNotNearOffice() => IsPlayerNearOffice = false;
     }
 }
