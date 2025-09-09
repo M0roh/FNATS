49f0a29c3e3f2fabdf502d61dc510aa8bcfb6016
Морон . <sisandr333@gmail.com>
2025-09-09 10:45:05 +0300

    Added pause ui

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        
+    }
+
+    public void Settings()
+    {
+        
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        
+    }
+}
