39ca8f432a4c28723e9ff058a041376ab43f2c39
M0roh <sisandr333@gmail.com>
2025-09-16 10:39:51 +0300

    Added brr brr patapim

---
+++ Assets/Scripts/Animatronics/BrrBrrPatapim.cs
@@ -0,0 +1,13 @@
+using VoidspireStudio.FNATS.Animatronics.Routes;
+
+namespace VoidspireStudio.FNATS.Animatronics
+{
+    public class BrrBrrPatapim : AnimatronicAI
+    {
+        protected override void OfficeAtack()
+        {
+        }
+
+        protected override void PerformSabotage(SabotageStep _) { }
+    }
+}
